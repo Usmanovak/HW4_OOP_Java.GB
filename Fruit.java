d048facc04e695521cf8d18668a9d7516757fb91
Karina <usmkarin@gmail.com>
2023-09-11 11:49:46 +0700

    done

---
+++ Fruit.java
@@ -0,0 +1,12 @@
+public abstract class Fruit {
+    
+    private final float weight;
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+}
